a71d91c5f88a07fa5be21261c0065e05f97b3af5
Eyosiyas Tadele <tdjosy@gmail.com>
2025-05-20 15:35:21 +0300

    disabled email sns service

--- src/application/events/user/handlers/user-registered.handler.ts
+++ src/application/events/user/handlers/user-registered.handler.ts
@@ -1,9 +1,10 @@
 import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
-import { UserRegisteredEvent } from "../user-registered.event";
+import { Logger } from "@nestjs/common";
+
 import { AWSConfigService } from "infrastructure/config/aws.config";
 import { VerificationTokenService } from "infrastructure/services/verification-token.service";
 import { NotificationTemplateService } from "infrastructure/services/notification-template.service";
-import { Logger } from "@nestjs/common";
+import { UserRegisteredEvent } from "../user-registered.event";
 
 @EventsHandler(UserRegisteredEvent)
 export class UserRegisteredHandler
@@ -25,23 +26,23 @@ export class UserRegisteredHandler
       );
 
       // Get email and SMS templates
-      const emailTemplate = this.notificationTemplateService.getEmailTemplate(
-        event.name,
-        token
-      );
+      // const emailTemplate = this.notificationTemplateService.getEmailTemplate(
+      //   event.name,
+      //   token
+      // );
       const smsTemplate =
         this.notificationTemplateService.getSMSTemplate(token);
 
       // Send email
-      await this.awsConfigService.sendEmail({
-        Destination: { ToAddresses: [event.email] },
-        Message: {
-          Body: { Html: { Data: emailTemplate } },
-          Subject: { Data: "Verify Your Email and Get Started!" },
-        },
-        Source: process.env.AWS_SES_FROM_EMAIL,
-      });
-      this.logger.log(`Email sent to ${event.email}`);
+      // await this.awsConfigService.sendEmail({
+      //   Destination: { ToAddresses: [event.email] },
+      //   Message: {
+      //     Body: { Html: { Data: emailTemplate } },
+      //     Subject: { Data: "Verify Your Email and Get Started!" },
+      //   },
+      //   Source: process.env.AWS_SES_FROM_EMAIL,
+      // });
+      // this.logger.log(`Email sent to ${event.email}`);
 
       // Send SMS
       await this.awsConfigService.sendSMS({
